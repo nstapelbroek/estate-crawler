86c75f0b074bb9a564680343d884964bf876a964
Nico Stapelbroek <nstapelbroek@gmail.com>
2016-08-30 22:58:33 +0200

    Move the string formatting to different methods, making the code more readable

--- nederwoon.py
+++ nederwoon.py
@@ -1,27 +1,63 @@
+# coding=utf-8
+import os
 import scrapy
 from urlparse import urlparse
 from scrapy.selector import Selector
+from configobj import ConfigObj
+from htmllaundry import strip_markup
+
 
 class NederwoonSpider(scrapy.Spider):
     name = 'nederwoonspider'
-    start_urls = ['http://www.nederwoon.nl/huurwoningen/amersfoort?numberviews=1000&rows=1000&q=']
+    config = ConfigObj(os.getcwd() + '/config.ini')
+    start_urls = [('http://www.nederwoon.nl/huurwoningen/{0}?numberviews=1000&rows=1000&q='.format(config['place']))]
+
+    def getConfig(self):
+        return ConfigObj(os.getcwd() + '/config.ini')
 
     def parse(self, response):
         pageSelector = Selector(response)
-        objects = pageSelector.xpath('//*[@id="cont_element75"]')
+        objects = pageSelector.css('form#searchform div#cont_element75')
+        objects.extract()
 
         for index, object in enumerate(objects):
-            # Determine if object is worth further investigating
-            priceText = object.xpath('//*[@class="price_red"]/text()').extract()[index].strip().replace('.','').replace(',', '.')
-            priceValue = float(priceText[2:])
+            #Determine if the object is the right type
+            type = str(self.extractString(object, 'div[data-object-kenmerk="type"]'))
+            if type != 'appartement' and type != 'kamer':
+                continue
 
-            if priceValue > 350.0 and priceValue < 900.0:
-                # Parse Path and send another request
-                path = object.xpath('//*[@class="btn_green"]/@onclick').extract()[index]
-                path = path.replace(' ', '').replace('document.location.href=\'', '').replace('?sighting=true\'', '')
-                parsed_uri = urlparse(response.url)
-                domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-                yield scrapy.Request(domain + path, self.parse_object)
+            # Parse Path and send another request
+            path = object.css('.btn_green').re_first(r'href=\'\s*(.*)\?sighting=true')
+            parsed_uri = urlparse(response.url)
+            domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+            yield scrapy.Request(domain + path, self.parse_object)
 
     def parse_object(self, response):
-            yield {'url': response.url}
+        street = self.extractString(response, '[data-object-kenmerk="straatnaam"]')
+        volume = self.extractString(response, '[data-object-kenmerk="woonoppervlakte"]').replace('m2', '')
+        rooms = self.extractString(response, '[data-object-kenmerk="slaapkamers"]')
+        availability = self.extractString(response, '[data-object-kenmerk="beschikbaarheid"]')
+        price = self.extractEurosAsFloat(response, '.price_red')
+
+        yield {
+            'street': street,
+            'volume': volume,
+            'rooms': rooms,
+            'availability': availability,
+            'price': price,
+            'reference': response.url
+        }
+
+    def extractEurosAsFloat(self, html, cssSelector):
+        data = self.extractString(html, cssSelector)
+        data = data.replace('.', '').replace(',', '.').replace('€', '')
+        return float(data)
+
+    def extractString(self, html, cssSelector):
+        if not isinstance(html, Selector):
+            html = Selector(html)
+
+        data = html.css(cssSelector).extract_first()
+        data = str(strip_markup(data).encode('utf-8'))
+        return data.strip()
+
