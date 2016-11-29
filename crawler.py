c993b175a3542a3dc9bda5e9c80eb846676b03b9
Nico Stapelbroek <nstapelbroek@gmail.com>
2016-11-29 14:41:00 +0100

    Add a script handler to call all spiders from cli

---
+++ crawler.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor, defer
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapper.spider.domica import DomicaSpider
+from scrapper.spider.eervast import EervastSpider
+from scrapper.spider.nederwoon import NederwoonSpider
+
+configure_logging()
+settings = get_project_settings()
+settings.overrides['FEED_FORMAT'] = 'jsonlines'
+settings.overrides['FEED_URI'] = 'build/result.json'
+runner = CrawlerRunner(settings)
+
+@defer.inlineCallbacks
+def crawl():
+    yield runner.crawl(DomicaSpider)
+    yield runner.crawl(EervastSpider)
+    yield runner.crawl(NederwoonSpider)
+    reactor.stop()
+
+crawl()
+reactor.run()
