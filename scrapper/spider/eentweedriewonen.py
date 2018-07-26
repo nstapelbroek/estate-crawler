fe0edba560ac0d4d483535ccc8d3c41692606fb1
Nico Stapelbroek <nstapelbroek@gmail.com>
2018-07-26 10:38:50 +0200

    Update spider for 123wonen.nl to function on the new site

--- scrapper/spider/eentweedriewonen.py
+++ scrapper/spider/eentweedriewonen.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 import scrapy
 from scrapy.selector import Selector, SelectorList
+from scrapy.http import FormRequest
 from scrapper.util.extractor import Extractor
 from scrapper.util.structure import Structure
-
 
 class EenTweeDrieWonenSpider(scrapy.Spider):
     name = '123WonenSpider'
@@ -11,67 +11,82 @@ class EenTweeDrieWonenSpider(scrapy.Spider):
 
     def __init__(self, queryRegion='Amersfoort'):
         self.region = queryRegion.title()
-        self.start_urls = [
-            (
-                'https://www.123wonen.nl/aanbod?city={0}&radius=10&min_price=&max_price=&size_m2=&per-page=50'
-                .format(queryRegion)
+
+    def start_requests(self):
+        # 123wonen.nl url's do not determine the content. The query results are fetched
+        # from the session storage which we can influence by POSTing a new query
+        return [
+            FormRequest(
+                "https://www.123wonen.nl/huurwoningen",
+                formdata={
+                    'cm01': '1',
+                    'redirecturl': 'huurwoningen',
+                    'pricerange': '-',
+                    'city': self.region,
+                    'zipcoder': '0',
+                    'price_start': '0',
+                    'price_end': '0'
+                },
+                callback=self.parse
             )
         ]
 
     def parse(self, response):
         pageSelector = Selector(response)
-        objects = pageSelector.css('.residence-item')
+        objects = pageSelector.css('.pandlist-container')
         objects.extract()
 
         for index, object in enumerate(objects):
             # Determine if the object is still available for rent
-            objectStatus = str(Extractor.string(object, '.status-label')).lower()
+            objectStatus = str(Extractor.string(object, '.pand-status')).lower()
             if objectStatus in ['verhuurd', 'in optie']:
                 continue
 
             # Skip crawling storage spaces and garages
-            type = Structure.find_in_definition(object, '.offer-detail > *', 'Type').lower()
-            if type in ['Garagebox', 'Berging/Opslag', 'Kantoorruimte', 'Loods', 'Parkeerplaats', 'Winkelpand']:
+            type = Structure.find_in_definition(object, '.pand-specs li > span', 'Type').lower()
+            if type in ['garagebox', 'berging/opslag', 'kantoorruimte', 'loods', 'parkeerplaats', 'winkelpand']:
                 continue
 
-            yield scrapy.Request(Extractor.url(response, object, '.button.button-orange::attr(href)'), self.parse_object)
+            yield scrapy.Request(
+                Extractor.string(object, 'a.textlink-design:contains("Details")::attr(href)'),
+                self.parse_object
+            )
+
+        # Crawl the next pages
+        nextPageSelector = '.productBrowser a:contains("volgende")'
+        nextPageLink = pageSelector.css(nextPageSelector).extract_first()
+        if nextPageLink is not None and isinstance(nextPageLink, basestring):
+            nextPageSelector += '::attr(href)'
+            yield scrapy.Request(
+                Extractor.url(response, pageSelector, nextPageSelector),
+                self.parse,
+            )
 
     def parse_object(self, response):
-        city_heading = Extractor.string(response, '.offer-detail-city').split(',')
-        city = Extractor.string(city_heading[0])
+        breadCrumbTitle = Extractor.string(response, 'a.active span').split(' - ')
+        city = Extractor.string(breadCrumbTitle[0])
+        breadCrumbTitle.pop(0)
+        street = ' - '.join(breadCrumbTitle)
 
-        # The site adds (nieuw) to a streetname whenever the object is considered new.
-        street_heading = Extractor.string(response, '.offer-detail-street').split('(nieuw)')
-        street = Extractor.string(street_heading[0])
-
-        volume = Structure.find_in_definition(response, '.offer-detail-container .offer-detail > *', 'Woonoppervlakte')
+        volume = Structure.find_in_definition(response, '.pand-specs.panddetail-desc li > span', 'Woonoppervlakte')
         if volume is not None and isinstance(volume, basestring):
             volume = Extractor.volume(volume)
 
-        rooms = Structure.find_in_definition(response, '.offer-detail-container .offer-detail > *', 'Slaapkamers')
+        rooms = Structure.find_in_definition(response, '.pand-specs.panddetail-desc li > span', 'Kamers')
         if rooms is not None and isinstance(rooms, basestring):
             Extractor.string(rooms)
 
-        type = Structure.find_in_definition(response, '.offer-detail-container .offer-detail > *', 'Type')
+        type = Structure.find_in_definition(response, '.pand-specs.panddetail-desc li > span', 'Type')
         if type is not None and isinstance(type, basestring):
             Extractor.string(type)
 
-        price = Structure.find_in_definition(response, '.offer-detail-container .offer-detail > *', 'Huurprijs')
-        if price is not None and isinstance(type, basestring):
-            price = Extractor.euro(price)
+        price = Extractor.string(response, '.panddetail-price')
+        price = price.split('-')[0]
+        price = Extractor.euro(price)
 
-        # If the object is available for rental right now, the site will give a DT without matching DD
-        right_now_element = response.xpath("//dt[@class='offer-value'][contains(text(),'Per direct beschikbaar')]")
-        if isinstance(right_now_element, SelectorList) and len(right_now_element) > 0:
-            availability = 'Per Direct'
-        else:
-            availability = Structure.find_in_definition(
-                response,
-                '.offer-detail-container .offer-detail > *',
-                'Beschikbaar vanaf'
-            )
-            if availability is not None and isinstance(type, basestring):
-                availability = Extractor.string(availability)
+        availability = Structure.find_in_definition(response, '.pand-specs.panddetail-desc li > span', 'Beschikbaarheid')
+        if availability is not None and isinstance(type, basestring):
+            availability = Extractor.string(availability)
 
         yield {
             'street': street,
@@ -84,5 +99,5 @@ class EenTweeDrieWonenSpider(scrapy.Spider):
             'pricePerMonth': price,
             'reference': Extractor.urlWithoutQueryString(response),
             'estateAgent': '123Wonen.nl',
-            'images': Extractor.images(response, '.highslide-gallery > a::attr(href)', True, 'https:'),
+            'images': Extractor.images(response, 'a[data-fancybox="group1"]::attr(href)', True),
         }
